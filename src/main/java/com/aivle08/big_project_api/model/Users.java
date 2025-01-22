08fb8136f9266585bb57ddd4efbb70d9809ea1da
ChaneHaDa <chane.ha.da@gmail.com>
2025-01-22 16:14:09 +0900

    feat: add jsonignore

--- src/main/java/com/aivle08/big_project_api/model/Users.java
+++ src/main/java/com/aivle08/big_project_api/model/Users.java
@@ -1,5 +1,6 @@
 package com.aivle08.big_project_api.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -17,6 +18,7 @@ public class Users {
     @Column(nullable = false, unique = true)
     private String username;
     @Column(nullable = false)
+    @JsonIgnore
     private String password;
     @Column(nullable = false)
     private String name;
