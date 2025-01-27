76466712206d5bc5498e96cae795df39a0505151
ChaneHaDa <chane.ha.da@gmail.com>
2025-01-27 22:24:19 +0900

    feat: change exception handler

--- src/main/java/com/aivle08/big_project_api/exception/GlobalExceptionHandler.java
+++ src/main/java/com/aivle08/big_project_api/exception/GlobalExceptionHandler.java
@@ -1,17 +1,35 @@
 package com.aivle08.big_project_api.exception;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.Builder;
+import lombok.Getter;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.time.LocalDateTime;
+import java.util.Map;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
     @ExceptionHandler(IllegalArgumentException.class)
-    public ResponseEntity<ErrorDetails> handleIllegalArgumentException(IllegalArgumentException ex) {
-        ErrorDetails errorDetails = new ErrorDetails(LocalDateTime.now(), ex.getMessage(), "Invalid argument provided");
-        return new ResponseEntity<>(errorDetails, HttpStatus.BAD_REQUEST);
+    public ResponseEntity<ErrorResponse> handleIllegalArgumentException(IllegalArgumentException ex) {
+        ErrorResponse response = ErrorResponse.builder()
+                .timestamp(LocalDateTime.now())
+                .code("Invalid argument provided")
+                .message(ex.getMessage())
+                .build();
+        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+    }
+
+    @Getter
+    @Builder
+    private static class ErrorResponse {
+        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
+        private LocalDateTime timestamp;
+        private String code;
+        private String message;
+        private Map<String, Object> details;
     }
 }
