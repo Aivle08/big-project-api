c5febaecb13d8259e2e5ff9b9bafb41c3d70a440
ChaneHaDa <chane.ha.da@gmail.com>
2025-01-27 22:19:54 +0900

    feat: remove cross origin annotation and add swagger setting

--- src/main/java/com/aivle08/big_project_api/controller/ApplicantController.java
+++ src/main/java/com/aivle08/big_project_api/controller/ApplicantController.java
@@ -2,13 +2,20 @@ package com.aivle08.big_project_api.controller;
 
 import com.aivle08.big_project_api.dto.input.ApplicantInputDTO;
 import com.aivle08.big_project_api.service.ApplicantService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+// TODO: 예외처리 추가 및 반환 값 지정
+
 @RestController
 @RequestMapping("/api/v1/recruitment/{id}")
+@Tag(name = "Applicant API", description = "지원자 조회 API")
 public class ApplicantController {
     private final ApplicantService applicantService;
 
@@ -17,15 +24,24 @@ public class ApplicantController {
     }
 
     @GetMapping("/applicant")
-    public ResponseEntity<?> getApplicantsByRecruitmentId(@PathVariable Long id) {
-
+    @Operation(summary = "지원자 리스트 조회")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "지원자 리스트 조회 성공"),
+            @ApiResponse(responseCode = "400", description = "잘못된 요청")
+    })
+    public ResponseEntity<List<ApplicantInputDTO>> getApplicantsByRecruitmentId(@PathVariable Long id) {
         List<ApplicantInputDTO> applicantsInputDTO = applicantService.getApplicantsByRecruitmentId(id);
 
         return ResponseEntity.ok().body(applicantsInputDTO);
     }
 
     @PostMapping
-    public ResponseEntity<?> postRecruitment(@RequestBody ApplicantInputDTO applicantInputDTO, @PathVariable Long id) {
+    @Operation(summary = "지원자 리스트 저장")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "지원자 리스트 저장 성공"),
+            @ApiResponse(responseCode = "400", description = "잘못된 요청")
+    })
+    public ResponseEntity<ApplicantInputDTO> postRecruitment(@RequestBody ApplicantInputDTO applicantInputDTO, @PathVariable Long id) {
         applicantService.applicantCreate(applicantInputDTO, id);
         return ResponseEntity.ok()
                 .body(applicantInputDTO);
--- src/main/java/com/aivle08/big_project_api/controller/RecruitmentController.java
+++ src/main/java/com/aivle08/big_project_api/controller/RecruitmentController.java
@@ -3,6 +3,10 @@ package com.aivle08.big_project_api.controller;
 import com.aivle08.big_project_api.dto.input.RecruitmentInputDTO;
 import com.aivle08.big_project_api.dto.output.RecruitmentOutputDTO;
 import com.aivle08.big_project_api.service.RecruitmentService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -10,7 +14,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/recruitment")
-@CrossOrigin
+@Tag(name = "Recruitment API", description = "채용 공고 조회 API")
 public class RecruitmentController {
 
     private final RecruitmentService recruitmentService;
@@ -20,12 +24,22 @@ public class RecruitmentController {
     }
 
     @GetMapping
+    @Operation(summary = "채용 공고 리스트 조회")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "공고 리스트 조회 성공"),
+            @ApiResponse(responseCode = "400", description = "잘못된 요청")
+    })
     public ResponseEntity<List<RecruitmentOutputDTO>> getRecruitments() {
         return ResponseEntity.ok()
                 .body(recruitmentService.findAllRecruitment());
     }
 
     @PostMapping
+    @Operation(summary = "지원자 리스트 조회")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "공고 리스트 저장 성공"),
+            @ApiResponse(responseCode = "400", description = "잘못된 요청")
+    })
     public ResponseEntity<RecruitmentInputDTO> postRecruitment(@RequestBody RecruitmentInputDTO recruitmentInputDTO) {
         recruitmentService.createRecruitment(recruitmentInputDTO);
         return ResponseEntity.ok()
