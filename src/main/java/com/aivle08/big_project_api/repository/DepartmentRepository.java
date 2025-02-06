94710057652040929625f8f925f6a1a94b43596a
ChaneHaDa <chane.ha.da@gmail.com>
2025-02-06 23:38:41 +0900

    feat: modify repository

--- src/main/java/com/aivle08/big_project_api/repository/DepartmentRepository.java
+++ src/main/java/com/aivle08/big_project_api/repository/DepartmentRepository.java
@@ -1,10 +1,7 @@
 package com.aivle08.big_project_api.repository;
 
-import com.aivle08.big_project_api.model.Company;
 import com.aivle08.big_project_api.model.Department;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
-
-public interface DepartmentRepository extends CrudRepository<Department, Long> {
+public interface DepartmentRepository extends JpaRepository<Department, Long> {
 }
