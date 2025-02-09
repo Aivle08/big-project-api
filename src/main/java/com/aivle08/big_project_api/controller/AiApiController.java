08a53ada86710fa25b7b6a827ceaa18aaa8ced09
ChaneHaDa <chane.ha.da@gmail.com>
2025-02-09 16:03:16 +0900

    feat: add question pipe

--- src/main/java/com/aivle08/big_project_api/controller/AiApiController.java
+++ src/main/java/com/aivle08/big_project_api/controller/AiApiController.java
@@ -1,5 +1,6 @@
 package com.aivle08.big_project_api.controller;
 
+import com.aivle08.big_project_api.dto.response.QuestionListResponseDTO;
 import com.aivle08.big_project_api.service.ApiPipeService;
 import com.aivle08.big_project_api.service.ApiService;
 import org.springframework.http.ResponseEntity;
@@ -7,6 +8,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/ai-api")
@@ -32,9 +35,11 @@ public class AiApiController {
     }
 
     @PostMapping("/{applicant-id}/question")
-    public ResponseEntity<String> postQuestionCall(@PathVariable(name = "applicant-id") Long applicantId) {
-        return ResponseEntity.accepted().body("Resume processing started.");
+    public ResponseEntity<List<QuestionListResponseDTO>> postQuestionCall(@PathVariable(name = "applicant-id") Long applicantId) {
+        List<QuestionListResponseDTO> questionListResponseDTOList = apiPipeService.questionPipe(applicantId);
+        return ResponseEntity.accepted().body(questionListResponseDTOList);
     }
+
 
     @PostMapping("/{recruitment-id}/score")
     public ResponseEntity<String> postScoreCall(@PathVariable(name = "recruitment-id") Long recruitmentId) {
@@ -45,6 +50,6 @@ public class AiApiController {
     @PostMapping("/{recruitment-id}/score-async")
     public ResponseEntity<String> postScoreCallAsync(@PathVariable(name = "recruitment-id") Long recruitmentId) {
         apiPipeService.scorePipeAsync2(recruitmentId);
-        return ResponseEntity.accepted().body("Score processing started.");
+        return ResponseEntity.ok().body("Score processing started.");
     }
 }
