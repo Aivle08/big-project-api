1836ee9ef84fc2dabce49c17cf5afacf4548a373
ChaneHaDa <chane.ha.da@gmail.com>
2025-02-17 11:17:46 +0900

    feat: add text limit

--- src/main/java/com/aivle08/big_project_api/model/EvaluationDetail.java
+++ src/main/java/com/aivle08/big_project_api/model/EvaluationDetail.java
@@ -13,6 +13,7 @@ public class EvaluationDetail {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column(length = 3000)
     private String summary;
 
     @OneToOne(mappedBy = "evaluationDetail")
